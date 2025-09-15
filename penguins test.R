161a0796450a9317e9baf36405a8c99e2274a2e9
James Dill <jcdill500@gmail.com>
2025-09-15 15:25:21 -0400

    Update penguins test.R

--- penguins test.R
+++ penguins test.R
@@ -1,6 +1,8 @@
+#James Dill
 library(tidyverse)
 library(palmerpenguins)
 
+#Using pipe operators and the ggplot2 package to create a histogram of penguin data
 penguins|>
   ggplot(aes(x=bill_depth_mm))+
   geom_histogram()
